0750c2e980dccc1e77150fe228c5dadb6201deb2
Ali Khalifeh <ali.khalife@podeo.co>
2025-11-16 15:28:37 +0200

    Add placeholder code to server and client +created project files and folders

---
+++ client.py
@@ -0,0 +1,5 @@
+def main():
+    print("Client running (placeholder).")
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,5 @@
+def main():
+    print("Server running (placeholder).")
+
+if __name__ == "__main__":
+    main()
